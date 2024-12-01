1d4a21e844474cc3264137283814cc9c9e9e8efd
Pedro Nordskog <73892999+pnpaiva@users.noreply.github.com>
2024-12-01 11:15:56 -0300

    Update providers structure

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,6 @@
 import { Inter } from 'next/font/google';
 import './globals.css';
-import { Providers } from './providers';
+import { AuthProvider } from '@/components/providers/session-provider';
 
 const inter = Inter({ subsets: ['latin'] });
 
@@ -17,7 +17,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.className}>
-        <Providers>{children}</Providers>
+        <AuthProvider>{children}</AuthProvider>
       </body>
     </html>
   );
